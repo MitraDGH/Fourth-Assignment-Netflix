a58d12a413b096d608c1d1d2dd104763adc8931d
Mitra Dehghan <79mitradehghan@gmail.com>
2023-03-15 23:15:20 +0330

    5.User

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -3,34 +3,84 @@ package org.example;
 import java.util.ArrayList;
 
 class User {
-    /*
-    * The user should contain username password.
-    * The user should contain an ArrayList of favorite shows and watch history.
-    * FUNCTION: the user should have a function to watch a show and add it to watch history.
-    *  *** NOTE: All search functions in user are for searching in favorite shows ***
-    */
+    private String username , password ;
+    private static ArrayList<TVShow> favoriteShows = new ArrayList<TVShow>();
+    private ArrayList<TVShow> watchHistory = new ArrayList<TVShow>();
+    public User (String username , String password , ArrayList<TVShow> favoriteShows ,  ArrayList<TVShow> watchHistory){
+        this.username = username;
+        this.password = password;
+        this.favoriteShows = favoriteShows;
+        this.watchHistory = watchHistory;
+    }
+    //***************************** Getters & Setters ************************************//
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public ArrayList<TVShow> getFavoriteShows() {
+        return favoriteShows;
+    }
+    public void setFavoriteShows(ArrayList<TVShow> favoriteShows) {
+        this.favoriteShows = favoriteShows;
+    }
+    public ArrayList<TVShow> getWatchHistory() {
+        return watchHistory;
+    }
+    public void setWatchHistory(ArrayList<TVShow> watchHistory) {
+        this.watchHistory = watchHistory;
+    }
+    //****************************
 
+    //**************************** Search Methods *******************************//
+    public static TVShow searchByTitle(String title) {
+        int i =0;
+        while(i<favoriteShows.size()){
+            if(favoriteShows.get(i).getTitle().equals(title)){
+                return favoriteShows.get(i);
+            }
+            i++;
+        }
+        return null;
+    }
+    public static TVShow searchByGenre(String genre) {
+        int i =0;
+        while(i<favoriteShows.size()){
+            if(favoriteShows.get(i).getGenre().equals(genre)){
+                return favoriteShows.get(i);
+            }
+            i++;
+        }
+        return null;
+    }
+    public static TVShow searchByReleaseYear(int year) {
+        int i =0;
+        while(i<favoriteShows.size()){
+            if(favoriteShows.get(i).getReleaseYear()==year){
+                return favoriteShows.get(i);
+            }
+            i++;
+        }        return null;
+    }
+    //***************************************************************************//
 
-    public ArrayList<TVShow> searchByTitle(String title) {
-        // Implement search by title in favorite shows  logic here
-        return null;
+    public static void addToFavorites(TVShow show) {
+        favoriteShows.add(show);
     }
-    public ArrayList<TVShow> searchByGenre(String genre) {
-        // Implement search by genre in favorite shows  logic here
-        return null;
+
+    public static void viewFavorites() {
+        System.out.println(favoriteShows.toArray().toString());
     }
-    public ArrayList<TVShow> searchByReleaseYear(int year) {
-        // Implement search by release year in favorite shows logic here
-        return null;
-    }
-    public void addToFavorites(TVShow show) {
-        // Implement add to favorites logic here
-    }
-    public void viewFavorites() {
-        // Implement view favorites logic here
-    }
-    public ArrayList<TVShow> getRecommendations() {
+    public ArrayList<TVShow> getRecommendations() {///?????????
         // Implement get recommendations logic here
         return null;
     }
+
 }

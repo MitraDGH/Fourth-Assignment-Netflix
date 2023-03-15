43ba3e6f772edd3c415611ae5d81167dd4168ea6
Mitra Dehghan <79mitradehghan@gmail.com>
2023-03-15 23:14:29 +0330

    2.Movie

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -3,11 +3,20 @@ package org.example;
 import java.util.ArrayList;
 
 class Movie extends TVShow {
-    /*
-     *Movie is extended from TVShow and has extra attribute length.
-     */
-    public Movie()
+    private int length;
+    public Movie(String title, String genre, int releaseYear,int length , double rating, ArrayList<String> cast)
     {
-        super();
+        super(title, genre, releaseYear, rating, cast);
+        this.length = length ;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
     }
 }
+
+
